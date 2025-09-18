0e1fbcca6ca77232b79c283b49aac0c0f15d4a28
Anish29801 <edu.anish29801@gmail.com>
2025-09-18 18:46:40 +0530

    cors commit

---
+++ server/src/cors/corsConfig.ts
@@ -0,0 +1,7 @@
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "PATCH","DELETE"],
+  credentials: true,
+};
+
+export default corsOptions;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 import app from "./app";
+import cors from "cors";
+import corsOptions from "./cors/corsConfig";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
+
+
+
+app.use(cors(corsOptions));
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
