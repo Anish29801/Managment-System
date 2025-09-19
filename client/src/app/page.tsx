39b23d520a6c3cabc228c689b9b69107f3b48099
Anish29801 <edu.anish29801@gmail.com>
2025-09-19 18:05:20 +0530

    Dashboard Started

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,30 @@
-export default function Home() {
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useAuth } from "./context/AuthContext";
+
+const Dashboard: React.FC = () => {
+  const { user } = useAuth();
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    const hours = new Date().getHours();
+    if (hours < 12) {
+      setGreeting("Good Morning");
+    } else if (hours < 18) {
+      setGreeting("Good Afternoon");
+    } else {
+      setGreeting("Good Evening");
+    }
+  }, []);
+
   return (
-    <div className=""></div>
+    <div className="min-h-screen bg-gray-900 text-white p-6">
+      <h1 className="text-3xl font-semibold">
+        Hi, {user?.name || "There"} {greeting}
+      </h1>
+    </div>
   );
-}
+};
+
+export default Dashboard;
